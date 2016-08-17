e83a1ed715c2068b016b1158853e4ae48a1b6616
tichinose1-oa <toyoh.ichinose@unisys.co.jp>
2016-08-18 01:02:41 +0900

    leapjsをnpmで管理。適当なログを出力

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const controller = new Leap.Controller();
+controller.connect();
 
+controller.on('frame', frame => {
+    const hand = frame.hands[0];
+    if (!hand) return;
+
+    const finger = hand.fingers[0];
+    if (!finger) return;
+
+    console.log(`finger.type: ${finger.type}`);
+    console.log(`finger.dipPosition: ${finger.dipPosition}`);
+});
