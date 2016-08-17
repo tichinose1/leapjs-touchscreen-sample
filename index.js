3b5f6a380c99cea8d0e2ec2f5d579fd98a0e055e
tichinose1-oa <toyoh.ichinose@unisys.co.jp>
2016-08-18 01:20:25 +0900

    Redbull画像をLeap Motionを使って指で動かしてみた

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ controller.on('frame', frame => {
     const hand = frame.hands[0];
     if (!hand) return;
 
-    const finger = hand.fingers[0];
+    const finger = hand.fingers[1];
     if (!finger) return;
 
     console.log(`finger.type: ${finger.type}`);
     console.log(`finger.dipPosition: ${finger.dipPosition}`);
+
+    const guide = document.getElementById('hoge');
+    guide.style.left = 600 + finger.dipPosition[0] - guide.width + 'px';
+    guide.style.top = 600 - finger.dipPosition[1] - guide.height + 'px';
 });
